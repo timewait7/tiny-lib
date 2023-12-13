2d388f8e3656e49a06cbc86847b9cacd1ecae6ea
linfeng <1029776125@qq.com>
2023-12-13 23:40:31 +0800

    first commit

---
+++ src/main/java/com/tw/lib/TinyLibApplication.java
@@ -0,0 +1,13 @@
+package com.tw.lib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyLibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinyLibApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tw/lib/TinyLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tw.lib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyLibApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
